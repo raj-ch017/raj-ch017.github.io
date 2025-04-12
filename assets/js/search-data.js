bba0f149196da5abefa930db3772b9fd094190fc
raj-ch017 <96816530+raj-ch017@users.noreply.github.com>
2025-04-12 11:59:00 +0000

    Deploying to gh-pages from @ raj-ch017/raj-ch017.github.io@e291cd0778696e0b7235b175175e8727fb9e0c2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This page contains my latest curriculum vitae. You can take a look by scrolling through this page, or download a copy by clicking on the top pdf download button.",
