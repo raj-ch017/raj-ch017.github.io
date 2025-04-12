2b84dedfcf866bc330ab38a7343ea0235d103b8e
raj-ch017 <96816530+raj-ch017@users.noreply.github.com>
2025-04-12 12:06:19 +0000

    Deploying to gh-pages from @ raj-ch017/raj-ch017.github.io@f115dfbea49b2d7ecc16c0a128710f60be07fce4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,12 +402,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -447,6 +442,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-automated-mice-assay",
+          title: 'Automated Mice Assay',
+          description: "An automated assay to study cost-benefit analysis",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/automated_assay/";
             },},{
         id: 'social-email',
         title: 'email',
